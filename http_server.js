92b9c427673d4d9a190ebc977f14804152753e1b
liamkande <liamkande@icloud.com>
2023-08-19 17:42:27 -0700

    add routes,config and ui

--- http_server.js
+++ http_server.js
@@ -1,66 +1,55 @@
 // add http server
-// -----------------------
-// YOUR CODE
+const express = require('express')
+const app = express()
+const low     = require('lowdb');
+const fs      = require('lowdb/adapters/FileSync');
+const adapter = new fs('db.json');
+const db      = low(adapter);
 
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
 
-// configure express to serve static files from public directory
-// ------------------------------------------------------------------
-// YOUR CODE
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 // init the data store
-db.defaults({ posts: []}).write();
+db.defaults({ users: []}).write();
+
+//Data parser - used to parse post data
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // list posts
-app.get('/data', function(req, res){     
-
-    // YOUR CODE
-
+app.get('/data', function(req, res){
+    res.send(db.get('users').value())
 });
 
-// ----------------------------------------------------
-// add post - test using:
-//      curl http://localhost:3000/posts/ping/1/false
-// ----------------------------------------------------
-app.get('/posts/:title/:id/:published', function(req, res){
+// post route
+app.post('/test', function(req, res){
+console.log(req.body.username, req.body.password)
+res.send(req.body.username + " " + req.body.password)
+})
 
-    // YOUR CODE
+// add user
+app.post('/add', function (req, res) {
+    const user = {
+        'name': req.body.name,
+        'dob': req.body.dob,
+        'email': req.body.email,
+        'username': req.body.username,
+        'password': req.body.password,
+        'phone': req.body.phone,
+        'streetaddress': req.body.streetaddress,
+        'citystatezip': req.body.citystatezip,
+        'latitude': req.body.latitude,
+        'longitude': req.body.longitude,
+        'avatar': req.body.avatar
+    };
 
+    db.get('users').push(user).write();
+    console.log(db.get('users').value());
+    res.send(db.get('users').value());
 });
 
-// ----------------------------------------------------
-// filter by published state - test using:
-//      curl http://localhost:3000/published/true
-// ----------------------------------------------------
-app.get('/published/:boolean', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// update published value - test using:
-//      curl http://localhost:3000/published/1/true
-// ----------------------------------------------------
-app.get('/published/:id/:boolean', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// delete entry by id - test using:
-//      curl http://localhost:3000/delete/5
-// ----------------------------------------------------
-app.get('/delete/:id/', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// start server
-// -----------------------
-// YOUR CODE
+app.listen(3000, function (){
+    console.log('Running on 3000')
+})
